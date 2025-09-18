8207e87e6bfb3045cb1321d94df3f74a9969441b
phloi183 <phloi12345674@gmail.com>
2025-09-18 17:50:48 +0700

    Add implementation for calculating average score from three subjects

---
+++ chuong2_cau3.py
@@ -0,0 +1,10 @@
+#Trả lời câu hỏi số 3 chương 2
+print("Chuong trinh tinh diem trung binh")
+toan=eval(input("Nhap diem toan: "))
+ly=eval(input("Nhap diem ly: "))
+hoa=eval(input("Nhap diem hoa: "))
+print("Diem toan", toan)
+print("Diem ly", ly)
+print("Diem hoa", hoa)
+dtb=(toan+ly+hoa)/3
+print("Diem trung binh cua 3 mon la: ",dtb)
