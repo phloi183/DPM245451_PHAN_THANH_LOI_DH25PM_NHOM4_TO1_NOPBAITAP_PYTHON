4b7b915853f16a1ff5302ad493d6a4c4cf2ef4a3
phloi183 <phloi12345674@gmail.com>
2025-10-02 08:15:10 +0700

    Add implementation for question 3 of chapter 3, solving quadratic equations

---
+++ Chuong3_cau3.py
@@ -0,0 +1,30 @@
+#trả lời câu hỏi số 3 chương 3
+
+from math import sqrt
+
+print("Chương trình Giải Phương trình bậc 2")
+a = float(input("Nhập a: "))
+b = float(input("Nhập b: "))
+c = float(input("Nhập c: "))
+
+if a == 0:  
+    # bx + c = 0
+    if b == 0 and c == 0:
+        print("Vô số nghiệm")
+    elif b == 0 and c != 0:
+        print("Vô nghiệm")
+    else:
+        x = -c / b
+        print("Nghiệm x =", x)
+else:
+    delta = b**2 - 4*a*c
+    if delta < 0:
+        print("Vô nghiệm")
+    elif delta == 0:
+        x = -b / (2*a)
+        print("Nghiệm kép x1 = x2 =", x)
+    else:
+        x1 = (-b - sqrt(delta)) / (2*a)
+        x2 = (-b + sqrt(delta)) / (2*a)
+        print("x1 =", x1)
+        print("x2 =", x2)
---
+++ chuong3_cau4.py
@@ -0,0 +1,18 @@
+#giải bài tập chương 3 câu 4
+
+x = 3
+y = 5
+z = 7
+
+print("a)  x == 3 ->", x ==3)
+print("b) x<y -> ", x<y)
+print("c) x>= y->", x>=y)
+print("d) x<= y->", x<=y)
+print("e) x != y - 2  ->", x != y - 2)
+print("f) x < 10  ->", x < 10)
+print("g) x >= 0 and x < 10  ->", x >= 0 and x < 10)
+print("h) x < 0 and x < 10   ->", x < 0 and x < 10)
+print("i) x >= 0 and x < 2   ->", x >= 0 and x < 2)
+print("j) x < 0 or x < 10    ->", x < 0 or x < 10)
+print("k) x >= 0 or x < 10   ->", x >= 0 or x < 10)
+print("l) x < 0 or x > 10    ->", x < 0 or x > 10)
