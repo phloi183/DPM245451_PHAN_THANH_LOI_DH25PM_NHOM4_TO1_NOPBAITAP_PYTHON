8eb0db5d6166179341b5e58b5796f004a26321d4
phloi183 <phloi12345674@gmail.com>
2025-10-09 11:49:59 +0700

    Refactor BMI classification and risk assessment functions; add new file for chapter 4 exercise 4

--- BT_CHUONG4/Chuong4_cau3.py
+++ BT_CHUONG4/Chuong4_cau3.py
@@ -1,38 +1,36 @@
-# giải bài tập chuong 4 câu 3
-
-def BMI(height,weight):
+def BMI(height, weight ):
     return weight/(height**2)
 def PhanLoai(bmi):
     if bmi<18.5:
-        return "Gầy"
+        return "gầy"
+    elif    bmi<=24.9:
+        return "bình thường"
+    elif bmi<=29.9:
+        return "hơi béo"
+    elif bmi<=34.9: 
+        return "Béo Phì Cấp Độ 1" 
+    elif bmi<=39.9: 
+        return "Béo Phì Cấp Độ 2" 
+    else: 
+        return "Béo Phì Cấp độ 3" 
+def nguycobenh(bmi):
+    if bmi<18.5:
+        return "thấp"
     elif bmi<=24.9:
- return "Bình thường"
- elif bmi<=29.9:
- return "Hơi Béo"
- elif bmi<=34.9:
- return "Béo Phì Cấp Độ 1"
- elif bmi<=39.9:
- return "Béo Phì Cấp Độ 2"
- else:
- return "Béo Phì Cấp độ 3"
-def NguyCoBenh(bmi):
- if bmi<18.5:
- return "Thấp"
- elif bmi<=24.9:
-return "Trung Bình"
- elif bmi<=29.9:
- return "Cao"
- elif bmi<=34.9:
- return "Cao"
- elif bmi<=39.9:
- return "Rất cao"
- else:
- return "Nguy Hiểm"
-print("Nhập vào chiều cao:")
-height=float(input())
-print("Nhập vào cân nặng:")
+          return "Trung Bình" 
+    elif bmi<=29.9: 
+        return "Cao" 
+    elif bmi<=34.9: 
+        return "Cao" 
+    elif bmi<=39.9: 
+        return "Rất cao" 
+    else: 
+        return "Nguy Hiểm" 
+print("nhập vào chiều cao:")
+height=float (input())
+print("nhập vào cân nặng: ")
 weight=float(input())
 bmi=BMI(height,weight)
-print("BMI của bạn=",bmi)
-print("Phân loại bạn=",PhanLoai(bmi))
-print("Nguy cơ bệnh của Thím=",NguyCoBenh(bmi))
+print("BMI của bạn=",bmi) 
+print("Phân loại bạn=",PhanLoai(bmi)) 
+print("Nguy cơ bệnh của Thím=",nguycobenh(bmi))
